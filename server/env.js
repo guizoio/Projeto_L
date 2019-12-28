43c1395d09ba977ea7876516a45588d79b149ff2
Gui Zoio <zoio.182@hotmail.com>
2019-12-28 00:15:42 -0300

    ESTRUTURA NODE

---
+++ server/env.js
@@ -0,0 +1,30 @@
+var config = {
+    teste_dev: {
+        url: 'http://localhost',
+        port: 3022,
+        ambiente: 'DEV',
+        session: {
+            secret: 'teste',
+            resave: false,
+            saveUninitialized: false,
+            cookie: { secure: false }
+        },
+        database: {
+            user: 'sa',
+            password: 'Ovodepascoades2',
+            server: '172.17.0.2',
+            database: 'ProjetoL',
+            requestTimeout: 180000,
+            connectionTimeout: 180000,
+            options: { encrypt: false },
+            pool: {
+                idleTimeoutMillis: 180000,
+                max: 100
+            }
+        }
+    }
+}
+
+exports.get = function get(env) {
+    return config.teste_dev;
+}
