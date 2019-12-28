d0e3174020ce8c8639346d6248ff2745ce229c3c
Gui Zoio <zoio.182@hotmail.com>
2019-12-27 21:53:03 -0300

    Alteração tala cadastro PC

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-function teste(){
-    alert("CLICO");
+$(document).ready(function() {
+    $('.load').hide();
+});
+
+function sleep (time) {
+    $('.load').show();
+    return new Promise((resolve) => setTimeout(resolve, time));
 }
+
+function teste(){
+    var login = document.getElementById("login").value;
+    alert("CLICO " + login);
+    sleep(2000).then(() => {
+        $(document).ready(function() {
+
+        });
+        window.location.assign("index.html");   
+    });
+    
+
+}
+
